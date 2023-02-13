7171f54a4faffac1e754312ec794e450b95ef38f
matyas17cz <116569487+matyas17cz@users.noreply.github.com>
2023-02-13 15:34:58 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,17 +6,27 @@ input.onButtonPressed(Button.B, function () {
 })
 radio.onReceivedValue(function (name, value) {
     if (name == "rychlost") {
-        wuKong.setAllMotor(value, value)
+        if (value > 0 && zastaveno == 1) {
+            wuKong.stopAllMotor()
+        } else {
+            wuKong.setAllMotor(value, value)
+            zastaveno = 0
+        }
     } else if (name == "smer") {
         wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, value)
     }
-    if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P15) < 30) {
-        wuKong.stopAllMotor()
-    }
 })
+let vzdalenost = 0
+let zastaveno = 0
 radio.setGroup(16)
 wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 180)
 wuKong.setAllMotor(0, 0)
+zastaveno = 0
 basic.forever(function () {
-	
+    vzdalenost = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_mm, DigitalPin.P2)
+    serial.writeValue("x", sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_mm, DigitalPin.P2))
+    if (vzdalenost > 0 && vzdalenost < 300) {
+        wuKong.stopAllMotor()
+        zastaveno = 1
+    }
 })
